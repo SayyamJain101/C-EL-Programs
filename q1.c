48e02e40012143b5eaea4cc50912dc8a503aa4b1
SayyamJain101 <110354530+SayyamJain101@users.noreply.github.com>
2023-03-10 20:28:58 +0530

    Update q1.c

--- q1.c
+++ q1.c
@@ -6,7 +6,9 @@ int main(){
     int arr[10] = {1,2,3,4,5,6,7,8,9,10};
     for(int i=0;i<10;i++){
         if(n==arr[i]){
-            printf("Yes it is present in the array!\nAt index %d",i);
+            printf("Yes the number is present in the array!\nAt index %d",i);
+        }else if(n!=arr[i] && i==9){
+            printf("No the number is not present in the array");
         }
     }
     return 0;
